be3f4fe9788d8c1824d663d4b4b5c97cddcf894e
GDIATTA <147615966+GDIATTA@users.noreply.github.com>
2024-09-25 23:54:17 +0000

    added two file agains

---
+++ inc_dec.py
@@ -0,0 +1,7 @@
+# increment : x+1
+def increment(x):
+    return x+1
+
+# decrement : x-1
+def decrement(x):
+    return x-1
---
+++ test.py
@@ -0,0 +1,9 @@
+import ind_dec as ind_dec
+
+# test for increment
+def test_increment():
+    assert ind_dec.increment(3)== 4
+
+# test for decrement
+def test_decrement():
+    assert ind_dec.decrement(3)== 2
