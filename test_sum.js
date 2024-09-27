e92c5d6a8c2e62e00ed110c243e55bd907c793ab
GDIATTA <147615966+GDIATTA@users.noreply.github.com>
2024-09-27 06:44:15 +0200

    Update test_sum.js

--- test_sum.js
+++ test_sum.js
@@ -1,6 +1,6 @@
-// sum.test.js
+// sum.test2.js
 const sum = require('./sum');  // Importing the module to test
 
-test('add should return 3 when input are 2 and 1', () => {
+test2('add should return 3 when input are 2 and 1', () => {
     expect(sum.add(2,1)).toBe(3);
 });
