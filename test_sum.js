315c3f61cca016045445f0b0b17bcf3628c4e09d
GDIATTA <147615966+GDIATTA@users.noreply.github.com>
2024-09-27 07:03:05 +0200

    Delete test_sum.js

--- test_sum.js
+++
@@ -1,6 +0,0 @@
-// sum.test2.js
-const sum = require('./sum');  // Importing the module to test
-
-test2('add should return 3 when input are 2 and 1', () => {
-    expect(sum.add(2,1)).toBe(3);
-});
